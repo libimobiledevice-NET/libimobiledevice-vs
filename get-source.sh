1148b410e444cbcca99a352bf8ee2c4fcc4f56d7
waynebonc <24826299+waynebonc@users.noreply.github.com>
2022-10-20 14:59:18 +0200

    New repo for libplist

--- get-source.sh
+++ get-source.sh
@@ -1,4 +1,4 @@
-git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libplist
+git clone --single-branch --branch msvc/master https://github.com/waynebonc/libplist
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libusbmuxd
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libimobiledevice
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libideviceactivation
