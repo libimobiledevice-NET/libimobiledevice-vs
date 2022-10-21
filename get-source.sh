26626a8f0c41144991a905d001d502542f464d58
waynebonc <24826299+waynebonc@users.noreply.github.com>
2022-10-21 11:09:26 +0200

    Update libirecovery repo

--- get-source.sh
+++ get-source.sh
@@ -3,7 +3,7 @@ git clone --single-branch --branch msvc/master https://github.com/waynebonc/libu
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libimobiledevice
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libideviceactivation
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/ideviceinstaller
-git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libirecovery
+git clone --single-branch --branch msvc/master https://github.com/waynebonc/libirecovery
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/idevicerestore
 git clone --single-branch --branch master-msvc https://github.com/libimobiledevice-win32/usbmuxd
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/ios-webkit-debug-proxy
